cbb6f2f264fcc5eebd0e1fb0a47040dcf49f89b8
Carl Tidelius <carl_tidelius@live.se>
2018-01-31 15:12:14 +0100

    update

--- src/GameEnvironment.java
+++ src/GameEnvironment.java
@@ -2,7 +2,7 @@
  * Created by carltidelius on 2018-01-29.
  */
 public interface GameEnvironment {
-    public void applyAction(int x, int y);
-    public boolean isLegalAction(int x, int y);
-    public void evalState();
+    void applyAction(int x, int y);
+    boolean isLegalAction(int x, int y);
+    void evalState();
 }
