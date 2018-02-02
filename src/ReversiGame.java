8d8368381563a7637219154dc178fb3789698732
Tom Andersen <elt15tan@beta-12.student.lth.se>
2018-02-02 10:58:39 +0100

    Minor changes in ReversiGame.java

--- src/ReversiGame.java
+++ src/ReversiGame.java
@@ -1,59 +1,67 @@
+import java.util.Set;
+
 /**
  * Created by carltidelius on 2018-01-19.
  */
 
-public class ReversiGame{
-    private GUI reversiGui;
-    private ReversiPiece[][] gameBoard;
+public class ReversiGame {
+	private GUI reversiGui;
+	private ReversiPiece[][] gameBoard;
 
-    public ReversiGame(){
-        reversiGui = new GUI();
-        gameBoard = new ReversiPiece[8][8];
-        initializeBoard();
-    }
-    
-    public ReversiGame copy() {
+	public final static int BLANK = 0, WHITE = 1, BLACK = 2;
+
+	public ReversiGame() {
+		reversiGui = new GUI();
+		gameBoard = new ReversiPiece[8][8];
+		initializeBoard();
+	}
+
+	public ReversiGame copy() {
 		return null;
-    }
+	}
 
-    /*
-     * Set up the game board with middle pieces as per Reversi rules
-     */
-    public void initializeBoard(){
-        for(int i = 0; i < 8; i++){
-            for (int j = 0; j < 8; j++){
-                if ((i == 3 && j == 3) || (i == 4 && j == 4)){
-                    gameBoard[i][j] = new ReversiPiece(1);
-                }else if ((i == 4 && j == 3) || (i == 3 && j == 4)){
-                    gameBoard[i][j] = new ReversiPiece(2);
-                }else{
-                    gameBoard[i][j] = new ReversiPiece(0);
-                }
-            }
-        }
-    }
-    /*
-    * Check if the action the player made on GUI is legal?
-     */
-    public boolean isLegalAction(int x, int y){
-        return true;
-    }
-    /*
-    * Apply the action that player made on GUI with XY coordinates?
-     */
-    public void applyAction(int x, int y){
+	/*
+	 * Set up the game board with middle pieces as per Reversi rules
+	 */
+	public void initializeBoard() {
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				if ((i == 3 && j == 3) || (i == 4 && j == 4)) {
+					gameBoard[i][j] = new ReversiPiece(WHITE);
+				} else if ((i == 4 && j == 3) || (i == 3 && j == 4)) {
+					gameBoard[i][j] = new ReversiPiece(BLACK);
+				} else {
+					gameBoard[i][j] = new ReversiPiece(BLANK);
+				}
+			}
+		}
+	}
 
-    }
-    /*
-    * Check the board to update GUI?
-     */
-    public void evalState(){
+	/*
+	 * Check if the action the player made on GUI is legal?
+	 */
+	public boolean isLegalAction(int x, int y) {
+		return true;
+	}
 
-    }
-    /*
-    * See all possible actions for player
-     */
-    public int[][] possMoves(){
-       return new int[8][8];
-    }
+	/*
+	 * Apply the action that player made on GUI with XY coordinates?
+	 */
+	public void applyAction(int x, int y) {
+
+	}
+
+	/*
+	 * Check the board to update GUI?
+	 */
+	public double evalState() {
+		return 0;
+	}
+
+	/*
+	 * See all possible actions for player
+	 */
+	public Set<ReversiPiece> getLegalActions(int color) {
+		return null;
+	}
 }
